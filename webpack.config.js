729b6a37b7bc6d783c323ba668ae555e230e8dd0
hagi <gardeer1541@gmail.com>
2023-06-07 15:13:43 +0530

    Add devServer to automatically load browser

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,28 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
         clean: true,
+        assetModuleFilename: "[name][ext]",
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
     },
     module: {
         rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, ],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
