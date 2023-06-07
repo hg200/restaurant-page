cc81b71a31b340955cb7ec21a095100495a85c5a
hagi <gardeer1541@gmail.com>
2023-06-07 18:21:14 +0530

    Create logic for switching between modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import generateHomePage from "./page-load"
+import generateInitialPage from "./page-load"
+import generateMenuPage from "./menu"
+import generateHomePage from "./home";
+import generateContactPage from "./contact"
 
+generateInitialPage();
+window.addEventListener("load", generateHomePage);
 
-generateHomePage();
+const navbar = document.querySelector(".navbar");
+
+const btnArr = Array.from(document.querySelectorAll(".btn"));
+const moduleGeneratorsArray = [generateMenuPage, generateHomePage, generateContactPage];
+
+for (let i = 0; i < btnArr.length; i++) {
+    btnArr[i].addEventListener("click", moduleGeneratorsArray[i], { once: true })
+}
